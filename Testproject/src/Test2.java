a0c88f57083152e296d4a43991c8cf681b4e4fc3
prabhat222 <prabhat.dwivedi121@gmail.com>
2020-03-01 20:24:56 +0530

    aoifjfjiahu

---
+++ Testproject/src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "prabhat dwivedi";
+		String y[]= str.split(" ");
+		System.out.println(y);
+	}
+
+	}
+
+
